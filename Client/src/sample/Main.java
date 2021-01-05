647b28afc6955b8fd4c4a5f919e1c9870943dd05
m-kempa <66827291+m-kempa@users.noreply.github.com>
2021-01-05 15:07:06 +0100

    Create starting page, but controller is not done

---
+++ Client/src/sample/Main.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+//Klasa główna inicująca okno logowania do systemu IPC
+public class Main extends Application {
+
+
+
+    public static boolean END = true;
+
+    //Metoda która rozpoczyna api (okno startowe)
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("IRC Panel Logowania");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 460, 296));
+        primaryStage.show();
+    }
+
+    //Metoda główna projektu, w niej w przyszłości będzie zamykane połączenie z serwerem
+    public static void main(String[] args) throws IOException {
+        launch(args);
+        END = false;
+
+    }
+}
