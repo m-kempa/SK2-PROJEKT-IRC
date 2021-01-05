190093bcee30f8b3413de5065077fafc4466fc06
m-kempa <66827291+m-kempa@users.noreply.github.com>
2021-01-05 14:58:35 +0100

    Server program starting sceleton

---
+++ Server/server.cpp
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <fstream>
+#include <unistd.h>
+#include <pthread.h>
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[]) {
+
+
+
+
+
+
+
+
+    return 0;
+}
