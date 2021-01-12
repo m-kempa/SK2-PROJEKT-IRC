fcf87439b467f217110c5886e7a99aa2c184b7f2
Lemaldo <45721425+Lemaldo@users.noreply.github.com>
2021-01-12 21:43:18 +0100

    client sends message to server about login

--- Client/src/sample/ConnectionToServer.java
+++ Client/src/sample/ConnectionToServer.java
@@ -35,23 +35,76 @@ public class ConnectionToServer {
         try {
             userSocket.connect(socketAddress, connection_limit);
         } catch (SocketTimeoutException ex) {
-            window.ErrorWindow("Błąd", "Zbyt długi czas oczekiwania",
-                    "Sprawdź poprawność operacji", Alert.AlertType.ERROR);
+            window.ErrorWindow("ERROR", "Zbyt długi czas oczekiwania", "Sprawdź poprawność operacji łączenia z serwerem", Alert.AlertType.ERROR);
             userSocket.close();
             return false;
         } catch (IOException e) {
-            window.ErrorWindow("Błąd", "Nie udało się podłączyć do servera",
-                    "Sprawdź poprawność operacji", Alert.AlertType.ERROR);
+            window.ErrorWindow("ERROR", "Połączenie z serwerem nie powiodło się", "Sprawdź poprawność operacji łączenia z serwerem", Alert.AlertType.ERROR);
             userSocket.close();
             return false;
         } catch (Exception e) {
-            window.ErrorWindow("Błąd", "Nie udało się podłączyć do servera",
-                    "Sprawdź poprawność operacji", Alert.AlertType.ERROR);
+            window.ErrorWindow("ERROR", "Połączenie z serwerem nie powiodło się", "Sprawdź poprawność operacji łączenia z serwerem", Alert.AlertType.ERROR);
             userSocket.close();
             return false;
         }
         return true;
     }
+
+    //Motoda wysyła do serwera nick użytkownika, serwer sprawdza unikalność, jeżeli użytkonik po podanym nicku już istnieje, serwer wysyła komunikat
+    public boolean IsNickUnique(String nick, ApiWidnowController window) throws IOException {
+
+        //Nie można zalogować się bez nicku
+        if (nick.replaceAll(" ", "").isEmpty()) {
+            window.ErrorWindow( "ERROR","Brak wypisanego pola Nick", "Pole Nick musi być wypełnione!", Alert.AlertType.ERROR);
+            return false;
+        }
+
+        //Podpatrzone z materiałów z labo
+        PrintWriter writer = new PrintWriter(userSocket.getOutputStream(), true);
+
+
+        String length = "";
+        length = NickZeroAdd(nick);
+
+        //Wysłanie komunikatu do serwera, kótry zawiera: login + długość nicku + nick użytkownika
+        writer.println("login" + length + nick);
+
+        //W bloku try cath otrzymujemy odpowiedź od serwera, który sprawdza unikalność nicku, w razie braku unikalności zostanie wyświetlony błąd
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(userSocket.getInputStream()));
+            String serverMessage = reader.readLine();
+
+            if (serverMessage.equals("0")) {
+                return true;
+            }
+            else if(serverMessage.equals("1")){
+                window.ErrorWindow("ERROR", "Zbyt duża ilość użytkowników", "Proszę poczekać", Alert.AlertType.ERROR);
+                userSocket.close();
+                return false;
+            }
+            else {
+                window.ErrorWindow("ERROR", "Podany Nick już istnieje", "Proszę wprowadzić inny Nick", Alert.AlertType.ERROR);
+                userSocket.close();
+                return false;
+            }
+        } catch (Exception in) {
+            window.ErrorWindow("ERROR", "Próba wysłania danych do serwera nie powiodła się.", "Spróboj ponownie, może serwer jest niedostępny.", Alert.AlertType.ERROR);
+            userSocket.close();
+            return false;
+        }
+    }
+
+
+    // metoda służąca uzupełnieniu długości nicku zerem jeżeli jego długość jest mnijsza do 10
+    private String NickZeroAdd(String login) {
+        String length;
+        if (login.length() < 10)
+            length = "0" + Integer.toString(login.length());
+        else
+            length = Integer.toString(login.length());
+        return length;
+    }
+
 
 
 
